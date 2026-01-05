8fd89db553d682c72b37f22c883cb9b5570a96f7
Leo Wu <r00922133@ntu.edu.tw>
2026-01-06 00:15:24 +0800

    feat: mv anchor (#15)
    * feat: adapt layout
    
    * config: build
    
    * chore: format

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/reward-back",
+  ...(isProduction && {
+    output: "export",
+    basePath: "/reward-back",
+  }),
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: "/reward-back",
+    NEXT_PUBLIC_BASE_PATH: isProduction ? "/reward-back" : "",
   },
 };
 
